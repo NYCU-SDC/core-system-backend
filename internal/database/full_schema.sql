b649ac5eb9bb0d3ad1ec3c8cb046345c1f92252f
amandahsieh <amandahsieh.926188@gmail.com>
2025-09-15 21:43:43 +0800

    fix: modify tenant schema's orgID reference table

--- internal/database/full_schema.sql
+++ internal/database/full_schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
 
 CREATE TABLE IF NOT EXISTS tenants
 (
-    id UUID PRIMARY KEY REFERENCES organizations(id) ON DELETE CASCADE,
+    id UUID PRIMARY KEY REFERENCES units(id) ON DELETE CASCADE,
     db_strategy db_strategy NOT NULL,
     owner_id UUID REFERENCES users(id) ON DELETE SET NULL
 );CREATE EXTENSION IF NOT EXISTS pgcrypto;
