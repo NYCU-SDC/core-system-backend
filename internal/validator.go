76a99ae469989d49230daaf24c82f643c4adb0dc
Tiffany <886fan@gmail.com>
2026-02-04 11:18:47 +0000

    feat: add font validate

--- internal/validator.go
+++ internal/validator.go
@@ -1,11 +1,23 @@
 package internal
 
 import (
+	"NYCU-SDC/core-system-backend/internal/form/font"
 	"github.com/go-playground/validator/v10"
 )
 
 func NewValidator() *validator.Validate {
-	return validator.New()
+	v := validator.New()
+
+	_ = v.RegisterValidation("font", func(fl validator.FieldLevel) bool {
+		id := fl.Field().String()
+		if id == "" {
+			return true
+		}
+		ok, err := font.IsValidID(id)
+		return err == nil && ok
+	})
+
+	return v
 }
 
 func ValidateStruct(v *validator.Validate, s interface{}) error {
