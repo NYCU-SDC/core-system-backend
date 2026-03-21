19cbb72be03c68bafe5d30f76546f56f747fe7c2
shuan <winniepooh.cs14@nycu.edu.tw>
2026-03-22 00:18:31 +0800

    fix: turn setup.yaml into setup.example.yaml

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -136,7 +136,7 @@ func main() {
 
 	validator := internal.NewValidator()
 	problemWriter := internal.NewProblemWriter()
-	_, err = setup.NewService(logger, dbPool, "setup.yaml")
+	_, err = setup.NewService(logger, dbPool, "setup.example.yaml")
 	if err != nil {
 		logger.Fatal("Failed to setup", zap.Error(err))
 	}
